d4779ff5fa40ef11ffe28a9a6a31a6fe00b8c6cd
pasquale daloiso <pasquale.daloiso@gmail.com>
2024-10-31 12:54:38 +0100

    design e scelta della tesi

--- prove_magenta/modello.py
+++ prove_magenta/modello.py
@@ -1,17 +1,59 @@
 import magenta
 from magenta.models import melody_rnn
 from magenta.music import midi_io, note_seq
+from magenta.models.melody_rnn import melody_rnn_sequence_generator
+from magenta.models.shared import sequence_generator_bundle
+from note_seq.protobuf import generator_pb2
+from note_seq.protobuf import music_pb2
 
 # Inizializza il modello
-bundle = melody_rnn.prepare_bundle('path/to/melody_rnn.mag')  # Specifica il percorso al tuo modello
-melody_rnn_model = melody_rnn.MelodyRnnModel(bundle)
+bundle = sequence_generator_bundle.read_bundle_file('/home/pasquale/Scrivania/tesi/prove_magenta/attention_rnn.mag')  # Specifica il percorso al tuo modello
+# Create the generator
+generator_map = melody_rnn_sequence_generator.get_generator_map()
+melody_rnn_generator = generator_map['attention_rnn'](checkpoint=None, bundle=bundle)
 
-# Configura i parametri per la generazione
-num_steps = 128  # Numero di note da generare
-temperature = 1.0  # Controlla la varietà della generazione
+# Create an empty melody as the input
+twinkle_twinkle = music_pb2.NoteSequence()
+# Add the notes to the sequence.
+twinkle_twinkle.notes.add(pitch=60, start_time=0.0, end_time=0.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=60, start_time=0.5, end_time=1.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=67, start_time=1.0, end_time=1.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=67, start_time=1.5, end_time=2.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=69, start_time=2.0, end_time=2.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=69, start_time=2.5, end_time=3.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=67, start_time=3.0, end_time=4.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=65, start_time=4.0, end_time=4.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=65, start_time=4.5, end_time=5.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=64, start_time=5.0, end_time=5.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=64, start_time=5.5, end_time=6.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=62, start_time=6.0, end_time=6.5, velocity=80)
+twinkle_twinkle.notes.add(pitch=62, start_time=6.5, end_time=7.0, velocity=80)
+twinkle_twinkle.notes.add(pitch=60, start_time=7.0, end_time=8.0, velocity=80)
+twinkle_twinkle.total_time = 8
 
-# Genera la sequenza musicale
-melody_sequence = melody_rnn_model.generate(num_steps, temperature)
+twinkle_twinkle.tempos.add(qpm=60);
+input_sequence = twinkle_twinkle # change this to teapot if you want
+num_steps = 128 # change this for shorter or longer sequences
+temperature = 1.0 # the higher the temperature the more random the sequence.
 
+# Set the start time to begin on the next step after the last note ends.
+last_end_time = (max(n.end_time for n in input_sequence.notes)
+                  if input_sequence.notes else 0)
+qpm = input_sequence.tempos[0].qpm
+steps_per_quarter = 4
+seconds_per_step = 60.0 / qpm / steps_per_quarter
+total_seconds = num_steps * seconds_per_step
+
+generator_options = generator_pb2.GeneratorOptions()
+generator_options.args['temperature'].float_value = temperature
+generate_section = generator_options.generate_sections.add(
+  start_time=last_end_time + seconds_per_step,
+  end_time=total_seconds)
+
+# Ask the model to continue the sequence.
+sequence = melody_rnn_generator.generate(input_sequence, generator_options)
+
+#note_seq.plot_sequence(sequence)
+#note_seq.play_sequence(sequence, synth=note_seq.fluidsynth)
 # Salva la sequenza in un file MIDI
-midi_io.note_sequence_to_midi_file(melody_sequence, 'melody.mid')
+midi_io.note_sequence_to_midi_file(sequence, 'melody.mid')
