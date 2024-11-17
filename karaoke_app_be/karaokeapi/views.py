a307c0fce860db93beb9c46ed8ccb15677da632c
pasquale <pasquale.daloiso@gmail.com>
2024-11-17 12:40:33 +0100

    login da testare e commentare nel doc

--- karaoke_app_be/karaokeapi/views.py
+++ karaoke_app_be/karaokeapi/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from rest_framework import filters
 
 from karaokeapi import serializer
@@ -93,3 +95,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permission.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email')
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
