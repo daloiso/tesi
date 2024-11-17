532c7a7c8ddf3bad80c4cf2c6e3ebe355ae83eee
pasquale <pasquale.daloiso@gmail.com>
2024-11-17 11:08:51 +0100

    viewset

--- karaoke_app_be/karaokeapi/urls.py
+++ karaoke_app_be/karaokeapi/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
 from karaokeapi import views
+from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+
+
+
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
